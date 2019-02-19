c578cea5e87887d4ec4a18f63e7a3b69555a9dd0
SakShiSivaRaM5 <42736943+SakShiSivaRaM5@users.noreply.github.com>
2019-02-19 21:09:59 +0530

    Update isomorphic.py

--- isomorphic.py
+++ isomorphic.py
@@ -1,9 +1,9 @@
-s,r=map(str,input().split())
-e=0
-for x in range(0,len(s)):
-    if((ord(s[x])-ord(s[x-1]))!=(ord(r[x])-ord(r[x-1]))):
-         e=e+1
-if(e>0):
+p,r=map(str,input().split())
+d=0
+for x in range(0,len(p)):
+    if((ord(p[x])-ord(p[x-1]))!=(ord(r[x])-ord(r[x-1]))):
+         d=d+1
+if(d>0):
       print ("no")
 else:
      print ("yes")
